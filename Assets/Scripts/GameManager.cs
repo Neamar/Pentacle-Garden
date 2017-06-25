03380774e45c4ed6dbf8228b3b51b0eacaca4e61
Matthieu Bacconnier <neamar@neamar.fr>
2017-06-25 17:17:16 +0100

    Node and vertex prefabs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance;
+
+	private LevelManager levelScript;
+
+	//Awake is always called before any Start functions
+	void Awake()
+	{
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			// Enforce Singleton pattern
+			Debug.Log("Non singleton GameManager created");
+			Destroy (gameObject);	
+		}
+
+		//Sets this to not be destroyed when reloading scene
+		DontDestroyOnLoad(gameObject);
+
+		levelScript = GetComponent<LevelManager>();
+
+		//Call the InitGame function to initialize the first level 
+		InitLevel();
+	}
+
+	void InitLevel() {
+		
+	}
+}
---
+++ Assets/Scripts/VertexDrawer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VertexDrawer : MonoBehaviour {
+	public GameObject input;
+	public GameObject output;
+
+	private LineRenderer lr;
+	// Use this for initialization
+	void Start () {
+		lr = gameObject.GetComponent<LineRenderer>();
+		lr.SetColors (Color.green, Color.green);
+
+		DrawLine (input.transform.position, output.transform.position, Color.blue);
+	}
+
+	void DrawLine(Vector3 start, Vector3 end, Color color)
+	{
+		lr.SetPosition(0, start);
+		lr.SetPosition(1, end);
+	}
+}
