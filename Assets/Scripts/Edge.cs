6923129b5a38340c7c593c3ff7e8d69a167dccae
Matthieu Bacconnier <neamar@neamar.fr>
2017-07-04 21:14:01 +0100

    Fix lineRenderer zOrder

--- Assets/Scripts/Edge.cs
+++ Assets/Scripts/Edge.cs
@@ -2,21 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Edge : MonoBehaviour {
+public class Edge : MonoBehaviour
+{
 	public Node input;
 	public Node output;
 
 	private LineRenderer lr;
 	// Use this for initialization
-	void Start () {
-		lr = gameObject.GetComponent<LineRenderer>();
+	void Start ()
+	{
+		lr = gameObject.GetComponent<LineRenderer> ();
 
 		DrawLine (input.gameObject.transform.position, output.gameObject.transform.position, Color.blue);
 	}
 
-	void DrawLine(Vector3 start, Vector3 end, Color color)
+	void DrawLine (Vector3 start, Vector3 end, Color color)
 	{
-		lr.SetPosition(0, start);
-		lr.SetPosition(1, end);
+		lr.SetPosition (0, start - Vector3.back);
+		lr.SetPosition (1, end - Vector3.back);
 	}
 }
